9b9b1ed58deb867572224917a4f4a1d76d437bfd
João Vitor R. da Silva <joaovitor.r.silva.3@gmail.com>
2026-02-06 20:55:59 -0300

    Atualiza arquivo entrypoint.sh

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -5,8 +5,7 @@ cd /var/www
 
 # 2. Corrige as permissões (O comando que você vivia digitando)
 # Ajusta o dono para www-data (usuário do PHP/Nginx)
-chown -R www-data:www-data /var/www
-
+chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
 # 3. Se a pasta vendor não existir, roda o install
 if [ ! -d "/var/www/vendor" ]; then
     echo "Instalando dependências..."
