e3621d09b17d0a5103b804dfbcc74ee76ece1d9a
Matheus <gollmannmatheus@gmail.com>
2026-02-12 18:21:56 -0300

    Refactor TicketPage component to improve UI layout and enhance comment display. Update user names in comments and adjust badge styling for ticket status, priority, and category. Add new utility function for conditional class names.

--- apps/web/app/(protected)/t/tickets/[id]/page.tsx
+++ apps/web/app/(protected)/t/tickets/[id]/page.tsx
@@ -7,6 +7,7 @@ import { Button } from "@/components/ui/button";
 import { ArrowLeftIcon, CalendarIcon, SendIcon, UserIcon } from "lucide-react";
 import Link from "next/link";
 import { Textarea } from "@/components/ui/textarea";
+import { cn } from "@/lib/utils";
 
 const ticket = {
     id: 1,
@@ -24,13 +25,13 @@ const comentarios = [
     {
         id: 1,
         content: "Comentário 1",
-        createdBy: "John Doe",
+        createdBy: "Usuario",
         createdAt: `${new Date().toLocaleDateString('pt-BR')} às ${new Date().toLocaleTimeString('pt-BR', { hour: '2-digit', minute: '2-digit' })}`,
     },
     {
         id: 2,
         content: "Comentário 2",
-        createdBy: "John Doe",
+        createdBy: "Admin",
         createdAt: `${new Date().toLocaleDateString('pt-BR')} às ${new Date().toLocaleTimeString('pt-BR', { hour: '2-digit', minute: '2-digit' })}`,
     },
 ]
@@ -39,14 +40,18 @@ export default function TicketPage() {
     const { id } = useParams<{ id: string }>();
     return (
         <>
-            <div className="flex flex-col gap-4 w-full justify-center items-center">
-                <div className="w-full max-w-2xl">
-                    <Link href="/dashboard">
+            <div className="flex flex-col gap-4 w-full items-center">
+                <div className="w-full max-w-2xl flex flex-col gap-2 items-start">
+                    <Link href="/t/dashboard">
                         <Button variant="outline" className="cursor-pointer">
                             <ArrowLeftIcon />
                             Voltar
                         </Button>
                     </Link>
+                    <div className="flex flex-row gap-4 items-center">
+                        <h2 className="text-2xl font-bold">Chamado #{id}</h2>
+                        <Badge variant="outline" className={cn(ticket.status === "Em aberto" ? "bg-blue-100 text-blue-500" : ticket.status === "Em andamento" ? "bg-yellow-100 text-yellow-500" : ticket.status === "Concluído" ? "bg-green-100 text-green-500" : "bg-red-100 text-red-500")}>{ticket.status}</Badge>
+                    </div>
                 </div>
                 {/* card ticket */}
                 <div className="w-full max-w-2xl">
@@ -61,14 +66,13 @@ export default function TicketPage() {
                         </CardHeader>
                         <CardContent>
                             <CardDescription>
-                                <div className="flex grid-cols-2 gap-2 mb-4">
-                                    <Badge variant="outline" className="bg-gray-100 text-gray-500">{ticket.status}</Badge>
-                                    <Badge variant="outline" className="bg-gray-100 text-gray-500">{ticket.priority}</Badge>
-                                    <Badge variant="outline" className="bg-gray-100 text-gray-500">{ticket.category}</Badge>
+                                <div className="flex flex-col gap-2 mb-4">
+                                    <span className="text-sm text-muted-foreground">Prioridade: <Badge variant="outline" className="bg-gray-100 text-gray-500">{ticket.priority}</Badge></span>
+                                    <span className="text-sm text-muted-foreground">Categoria: <Badge variant="outline" className="bg-gray-100 text-gray-500">{ticket.category}</Badge></span>
                                 </div>
                                 <div className="flex flex-col gap-2">
-                                    <div className="bg-gray-100 px-2 py-4 rounded-md mb-2">
-                                        <span className="text-sm">{ticket.description}</span>
+                                    <div className="bg-gray-50 px-2 py-4 rounded-md mb-2">
+                                        <span className="text-sm text-black">{ticket.description}</span>
                                     </div>
                                     <div className="flex items-center gap-8">
                                         <div className="flex items-center gap-2">
@@ -87,22 +91,20 @@ export default function TicketPage() {
                 </div>
                 {/* card comentarios */}
                 <div className="w-full max-w-2xl">
+                    <h3 className="text-lg font-bold mb-4">Comentários ({comentarios.length})</h3>
                     <Card>
-                        <CardHeader>
-                            <CardTitle>Comentários ({comentarios.length})</CardTitle>
-                        </CardHeader>
                         <CardContent>
                             <div className="flex flex-col gap-4 w-full">
                                 {/* card comentario */}
-                                <div className="flex flex-col gap-2 w-full">
+                                <div className="flex flex-col gap-2 w-full ">
                                     {comentarios.map((comentario) => (
-                                        <div key={comentario.id} className="flex flex-col justify-start gap-2 w-full bg-gray-100 p-4 rounded-lg">
+                                        <div key={comentario.id} className="flex flex-col gap-2 w-full bg-gray-100 p-4 py-6 rounded-lg">
                                             <div className="flex gap-2">
                                                 <UserIcon className="w-4 h-4" />
-                                                <span className="text-sm text whitespace-nowrap">{comentario.createdBy}</span>
+                                                <span className="text-sm font-medium text whitespace-nowrap">{comentario.createdBy}</span>
                                                 <span className="text-sm text-muted-foreground whitespace-nowrap">{comentario.createdAt}</span>
                                             </div>
-                                            <div className="ml-6">
+                                            <div className="ml-6 py-4">
                                                 <span className="text-sm w-full">{comentario.content}</span>
                                             </div>
                                         </div>
