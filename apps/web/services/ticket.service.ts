77e54b80dcad1e7c141ae4aa6c68b1756bfa4d7e
Matheus <gollmannmatheus@gmail.com>
2026-02-12 18:21:34 -0300

    test: Enhance ticketService with new methods for fetching user tickets, all ticket statuses, updating ticket status, and creating comments. Improve error handling for API interactions.

--- apps/web/services/ticket.service.ts
+++ apps/web/services/ticket.service.ts
@@ -1,44 +1,75 @@
+import { getAuthHeaders } from "@/lib/api";
+import { Ticket, TicketStatus } from "@/lib/types";
+import type { CreateCommentError, FetchError, UpdateTicketStatusError } from "@/lib/errors";
 import { ok, err, Result } from "neverthrow";
 
-// buscar endpoint na api
 
-type Ticket = {
-    id: string;
-    title: string;
-    description: string;
-    user_id: string;
-    assigned_to: string | null;
-    status: string;
-    priority: string;
-    created_at: number;
-};
+export const ticketService = {
+    async getAllTickets(): Promise<Result<Ticket[], FetchError>> {
+        try {
+            const headers = await getAuthHeaders()
+            const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/tickets`,
+                {
+                    headers,
+                }
+            )
 
-type FetchError = {
-    type: 'NOT_FOUND';
-    message: string;
-} | {
-    type: 'FETCH_ERROR';
-    message: string;
-} | {
-    type: 'UNKNOWN_ERROR';
-    message: string;
-}
+            if (res.status === 404) {
+                return err({ type: 'NOT_FOUND', message: 'No tickets found' });
+            }
 
-export async function getTickets(): Promise<Result<Ticket[], FetchError>> {
-    try {
-        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/tickets`);
+            if (!res.ok) {
+                return err({ type: 'FETCH_ERROR', message: 'Failed to fetch tickets' });
+            }
 
-        if (res.status === 404) {
-            return err({ type: 'NOT_FOUND', message: 'No tickets found' });
+            const data: Ticket[] = await res.json();
+            return ok(data);
+        } catch (error) {
+            return err({ type: 'UNKNOWN_ERROR', message: 'An unknown error occurred while fetching tickets' });
         }
+    },
+    async getUserTickets(userId: string): Promise<Result<Ticket[], FetchError>> {
+        try {
+            const headers = await getAuthHeaders()
+            const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/user/${userId}/tickets`,
+                {
+                    headers,
+                }
+            )
 
-        if (!res.ok) {
-            return err({ type: 'FETCH_ERROR', message: 'Failed to fetch tickets' });
+            return ok([]);
+        } catch (error) {
+            return err({ type: 'UNKNOWN_ERROR', message: 'An unknown error occurred while fetching the user tickets' });
         }
+    },
+    async getAllTicketsStatuses(): Promise<Result<TicketStatus[], FetchError>> {
+        try {
+            return ok([]);
+        } catch (error) {
+            return err({ type: 'UNKNOWN_ERROR', message: 'An unknown error occurred while fetching the ticket statuses' });
+        }
+    },
+    async updateTicketStatus(ticketId: string, status: TicketStatus): Promise<Result<true, UpdateTicketStatusError>> {
+        try {
+            return ok(true);
+        } catch (error) {
+            return err({ type: 'UNKNOWN_ERROR', message: 'An unknown error occurred while updating the ticket status' });
+        }
+    },
+    async createComment(ticketId: string, comment: string): Promise<Result<true, CreateCommentError>> {
+        try {
+            const headers = await getAuthHeaders()
+            const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/tickets/${ticketId}/comments`,
+                {
+                    headers,
+                    method: "POST",
+                    body: JSON.stringify({ comment }),
+                }
+            )
 
-        const data: Ticket[] = await res.json();
-        return ok(data);
-    } catch (error) {
-        return err({ type: 'UNKNOWN_ERROR', message: 'An unknown error occurred while fetching tickets' });
+            return ok(true);
+        } catch (error) {
+            return err({ type: 'UNKNOWN_ERROR', message: 'An unknown error occurred while creating the comment' });
+        }
     }
 }
